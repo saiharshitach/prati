536c5d57dbebba37630b6ecd21cc604e9659e375
goran <goran@ProBook>
2019-02-04 21:19:28 +0100

    početno

---
+++ src/main/java/rs/cybertrade/gps/prati/ApplicationContextProvider.java
@@ -0,0 +1,37 @@
+package rs.cybertrade.gps.prati;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component("applicationContextProvider")
+public class ApplicationContextProvider implements ApplicationContextAware{
+
+	private static class AplicationContextHolder{
+        private static final InnerContextResource CONTEXT_PROV = new InnerContextResource();
+        private AplicationContextHolder() {
+            super();
+        }
+    }
+
+    private static final class InnerContextResource {
+        private ApplicationContext context;
+        private InnerContextResource(){
+            super();
+        }
+
+        private void setContext(ApplicationContext context){
+            this.context = context;
+        }
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return AplicationContextHolder.CONTEXT_PROV.context;
+    }
+	@Override
+	public void setApplicationContext(ApplicationContext ac) throws BeansException {
+		AplicationContextHolder.CONTEXT_PROV.setContext(ac);
+	}
+
+}
---
+++ src/main/java/rs/cybertrade/gps/prati/Servis.java
@@ -0,0 +1,24 @@
+package rs.cybertrade.gps.prati;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@WebListener
+public class Servis implements ServletContextListener{
+	@SuppressWarnings("unused")
+	private ApplicationContext context;
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		context = new ClassPathXmlApplicationContext("applicationContext.xml");
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		context = null;
+	}
+
+}
